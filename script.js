91d5a05049559df53088e598010d1aca9e1eae02
Edith M <info@paperhearts.tech>
2020-05-19 02:04:16 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,46 @@
+/*
+Based in Tania Rascia's Ghibli app:
+https://github.com/taniarascia/sandbox/tree/master/ghibli
+*/
+
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.src = 'logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
+const apiURL = 'https://ghibliapi.herokuapp.com/films';
+
+//geting data
+fetch(apiURL)
+  .then(response => response.json())
+  .then(data => {
+    var films = data;
+    // film info
+    films.forEach(movie => {
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+
+      const h1 = document.createElement('h1');
+      h1.textContent = movie.title;
+      h1.textContent += ' (' + movie.director + ', ' + movie.release_date + ')';
+
+      const p = document.createElement('p');
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}...`;
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+    })
+  })
+  .catch(err => {
+    const errorMessage = document.createElement('marquee');
+    errorMessage.textContent = err;
+    app.appendChild(errorMessage);
+});
